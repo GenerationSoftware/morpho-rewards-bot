487b87e3cba672875547a28e66e6ee547b7e3baf
Charles Bergeron <chuck.bergeron@gmail.com>
2025-08-05 16:50:03 -0700

    getting 'NotWhitelisted()' error, which means we should be good to go as soon as the EOA we're using is allow-listed

--- run-claim.js
+++ run-claim.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const { createWalletClient, http, createPublicClient } = require('viem');
+const { http, createPublicClient } = require('viem');
 const { privateKeyToAccount } = require('viem/accounts');
 
 const CHAIN_ID = 480;
@@ -42,10 +42,6 @@ const CLAIM_ABI = [
     type: 'function',
   },
 ];
-// const abi = parseAbi([
-//   'function mint(uint256 tokenId)',
-//   'function balanceOf(address owner) view returns (uint256)',
-// ]);
 
 async function sendTransaction(claimData) {
   const privateKey = process.env.CUSTOM_RELAYER_PRIVATE_KEY;
@@ -60,22 +56,11 @@ async function sendTransaction(claimData) {
   const users = [USER_PRIZE_VAULT_ADDRESS];
   const tokens = [WORLD_TOKEN_ADDRESS];
   const amounts = [claimData.unclaimed];
-  const proofs = claimData.proof;
-
-  // const data = encodeFunctionData({
-  //   abi: CLAIM_ABI,
-  //   functionName: 'claim',
-  //   args: [users, tokens, amounts, proofs],
-  // });
+  const proofs = [claimData.proof];
 
   try {
     const account = privateKeyToAccount(`0x${privateKey}`);
     const publicClient = createPublicClient({
-      chain: 480, // You can specify a chain if needed, e.g., mainnet, goerli, etc.
-      transport: http(rpcUrl),
-    });
-    const walletClient = createWalletClient({
-      account,
       chain: 480, // You can specify a chain if needed, e.g., mainnet, goerli, etc.
       transport: http(rpcUrl),
     });
@@ -89,18 +74,12 @@ async function sendTransaction(claimData) {
       account,
     });
 
-    // const hash = await client.sendTransaction({
-    //   to: MERKL_DISTRIBUTOR_CONTRACT_ADDRESS,
-    //   data,
-    // });
-    // console.log(`Contract transaction sent! Hash: ${hash}`);
-
     // Execute the transaction
-    const hash = await walletClient.writeContract(request);
+    const hash = await publicClient.writeContract(request);
     console.log('Transaction hash:', hash);
 
     // Optionally, you can wait for the transaction receipt
-    const transactionReceipt = await walletClient.waitForTransactionReceipt({ hash });
+    const transactionReceipt = await publicClient.waitForTransactionReceipt({ hash });
     console.log('Transaction receipt:', transactionReceipt);
   } catch (err) {
     console.error('Contract transaction failed:', err);
@@ -115,12 +94,12 @@ async function run() {
   const claimData = response.data[CHAIN_ID].tokenData[WORLD_TOKEN_ADDRESS];
   const somethingToClaim = claimData.unclaimed !== '0';
 
-  // if (somethingToClaim) {
-  console.log('Sending claim tx!');
-  await sendTransaction(claimData);
-  // } else {
-  // console.log('Nothing to claim ...');
-  // }
+  if (somethingToClaim) {
+    console.log('Sending claim tx!');
+    await sendTransaction(claimData);
+  } else {
+    console.log('Nothing to claim ...');
+  }
 }
 
 run().catch(console.error);
